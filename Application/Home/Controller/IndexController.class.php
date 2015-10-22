e52598b83eb2513dcc67c9d55734f95fa09db849
xzjs <2310976219@qq.com>
2015-10-22 10:52:02 +0800

    修改数据库数据并修复session bug

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -23,30 +23,7 @@ class IndexController extends Controller
         $this->success('注销成功', __APP__);
     }
 
-    /**
-     * 增加商品
-     */
-    public function addx()
-    {
-        $upload = new \Think\Upload();// 实例化上传类
-        $upload->maxSize = 3145728;// 设置附件上传大小
-        $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath = './Uploads/'; // 设置附件上传根目录
-        $upload->savePath = ''; // 设置附件上传（子）目录
-        $upload->saveName = 'time';
-        $upload->autoSub  = false;
-        // 上传文件
-        $info = $upload->upload();
-        $Support=D("Support");
-        $Support->create();
-        if (!$info) {// 上传错误提示错误信息
-            $this->error($upload->getError());
-        } else {// 上传成功
-            $Support->photo=$info['photo']['savename'];
-            $Support->add();
-            $this->success('发布成功',"mysupport");
-        }
-    }
+
 
     /**
      * 我的供应
